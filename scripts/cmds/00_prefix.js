aaf09c9f944e077cf382223ec53dc4b10364fa42
Rafiyan Rifat <164067230+faru-100@users.noreply.github.com>
2025-01-16 19:11:01 +0600

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -15,12 +15,12 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
  body: `
-Yo, my prefix is [ 𓆩 _</>_ 𓆪 ]\n
+Yo, my prefix is [ 𓆩 _<.>_ 𓆪 ]\n
 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
 ➥ &help [number of page] -> see commands
 ➥ &sim [message] -> talk to bot
 ➥ &callad [message] -> report any problem encountered
-➥ &help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/brandtamim69`,
+➥ &help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: 𝗵𝘁𝘁𝗽𝘀://𝘄𝘄𝘄.𝗳𝗮𝗰𝗲𝗯𝗼𝗼𝗸.𝗰𝗼𝗺/𝗲𝘅𝗽𝗼𝘀𝗲.𝗿𝗶𝗳𝘂`,
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/KFCge3F.jpeg")
  });
  }
